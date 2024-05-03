eccd30318df888be54f17730fb9312d289ac4583
gizmore <gizmore@wechall.net>
2024-05-04 01:57:59 +0200

    'dog website'

--- module_dog_website.py
+++ module_dog_website.py
@@ -2,4 +2,9 @@ from gdo.base.GDO_Module import GDO_Module
 
 
 class module_dog_website(GDO_Module):
-    pass
+    """
+    The Dog Chatbot Website
+    """
+
+    def gdo_is_site_module(self) -> bool:
+        return True
