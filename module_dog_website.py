73cb9f8ce72f3cadd6f1e0febf88819c8f529006
Christian Busch <cb@oncrea.de>
2025-01-17 02:27:27 +0100

    'dog website dependencies'

--- module_dog_website.py
+++ module_dog_website.py
@@ -5,17 +5,19 @@ class module_dog_website(GDO_Module):
     """
     The Dog Chatbot Website
     """
-
     def gdo_is_site_module(self) -> bool:
         return True
 
     def gdo_dependencies(self) -> list:
         return [
             'blackjack',
-            'classic',
+            'bootstrap5',
+            'chatgpt4o',
             'contact',
+            'discord',
             'irc',
             'login',
+            'math',
             'perf',
             'register',
             'slapwarz',
